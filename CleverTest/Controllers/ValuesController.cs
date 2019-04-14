95c23ee78a97166b987655c6ebd6f52e4d17f7a6
Aghasi Manukyan <aghasim@yandex.ru>
2019-04-14 12:01:03 +0400

    Add project files.

---
+++ CleverTest/Controllers/ValuesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+
+
+namespace CleverTest.Controllers
+{
+    [Route("api/values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<int>> Get()
+        {
+            var res =new List<int>();
+            for (int i = 0; i < 10; i++)
+            {
+                res.Add(Server.GetCount());
+            }
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<int> Get(int id)
+        {
+            Server.AddToCount(id);
+            return Ok();
+        }
+
+    }
+}
---
+++ CleverTest/Server.cs
@@ -0,0 +1,38 @@
+﻿using System.Threading;
+
+namespace CleverTest
+{
+    public static class Server
+    {
+        private static ReaderWriterLockSlim rwl = new ReaderWriterLockSlim();
+        static int Count { get; set; } = 0;
+        public static int GetCount()
+        {
+            rwl.EnterReadLock();
+            try
+            {
+                Thread.Sleep(300);
+                return Count;
+            }
+            finally
+            {
+                rwl.ExitReadLock();
+            }
+        }
+
+
+        public static void AddToCount(int value)
+        {
+            rwl.EnterWriteLock();
+            try
+            {
+                Thread.Sleep(10000);
+                Count += value;
+            }
+            finally
+            {
+                rwl.ExitWriteLock();
+            }
+        }
+    }
+}
